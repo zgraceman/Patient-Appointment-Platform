dc59b545bf8b0ca3563fe00807945d201c05e235
ZachGraceman <zach.graceman@gmail.com>
2023-03-16 08:57:24 -0500

    JUnit testing

--- src/main/java/org/perscholas/mbs/dao/PatientRepoI.java
+++ src/main/java/org/perscholas/mbs/dao/PatientRepoI.java
@@ -11,4 +11,6 @@ import java.util.Optional;
 @Repository
 public interface PatientRepoI extends JpaRepository<Patient, Integer> {
     Optional<Patient> findByEmailAllIgnoreCase(String email);
+
+    //Optional<Patient> findByName(String name);
 }
--- src/test/java/org/perscholas/mbs/MbsNewApplicationTests.java
+++ src/test/java/org/perscholas/mbs/MbsNewApplicationTests.java
@@ -1,13 +1,26 @@
 package org.perscholas.mbs;
 
 import org.junit.jupiter.api.Test;
+import org.perscholas.mbs.dao.PatientRepoI;
+import org.perscholas.mbs.models.Patient;
+import org.perscholas.mbs.service.PatientService;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class MbsNewApplicationTests {
 
+	private PatientService patientService;
+
+
 	@Test
-	void contextLoads() {
+	public void verifyPatient() throws Exception{
+
+		Patient testUser = new Patient("User", "User@gmail.com", "password");
+		Patient expectedPatient = patientService.getAllPatients().get(0);
+
+		assertThat(testUser).isEqualTo(expectedPatient);
 	}
 
 }
