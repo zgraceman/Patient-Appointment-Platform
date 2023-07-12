5e92769eb7f8953d3ac4acb9f7c823fbc2b99d83
zgraceman <zach.graceman@gmail.com>
2023-07-12 14:41:04 -0500

    Initialize AppointmentController for refractoring

--- src/main/java/org/perscholas/mbs/controllers/AppointmentController.java
+++ src/main/java/org/perscholas/mbs/controllers/AppointmentController.java
@@ -1,4 +1,22 @@
 package org.perscholas.mbs.controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.perscholas.mbs.dao.AppointmentRepoI;
+import org.perscholas.mbs.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
 public class AppointmentController {
+
+    // Injecting the necessary services and repositories
+    private final AppointmentRepoI appointmentRepoI;
+    private final AppointmentService appointmentService;
+
+    @Autowired
+    public AppointmentController(AppointmentRepoI appointmentRepoI, AppointmentService appointmentService) {
+        this.appointmentRepoI = appointmentRepoI;
+        this.appointmentService = appointmentService;
+    }
 }
